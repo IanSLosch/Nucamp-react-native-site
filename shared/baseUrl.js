b05f929c1e1de2b04c405e9408a8178069a6c1a1
Ian S <ianslosch@gmail.com>
2023-05-06 13:20:40 -0400

    week 2 workshop - redux

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,8 +1,8 @@
 //this changes depending on the wifi network connected to
 
 //home
-// export const baseUrl = 'http://192.168.1.23:3001/'
+export const baseUrl = 'http://192.168.1.23:3001/'
 
 
 //coffee shop
-export const baseUrl = 'http://192.168.9.119:3001/'
+// export const baseUrl = 'http://192.168.9.119:3001/'
