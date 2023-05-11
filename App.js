640c826da4978cea2d4a9926ee086241b278321a
Ian S <ianslosch@gmail.com>
2023-05-10 23:20:17 -0400

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'; // prevents the application from loading until the redux store has rehydrated from the client side storage
+import Loading from './components/LoadingComponent';
 
+
+// whatever is passed to PersistGate's "loading=" prop will show when it isloading
 export default function App() {
     return (
         <Provider store={store}>
-            <NavigationContainer>
-                <Main />
-            </NavigationContainer>
+            <PersistGate loading={<Loading />} persistor={persistor} > 
+                <NavigationContainer>
+                    <Main />
+                </NavigationContainer>
+            </PersistGate>
         </Provider>
     );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,18 +1,50 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { campsitesReducer } from '../features/campsites/campsitesSlice'
 import { commentsReducer } from '../features/comments/commentsSlice'
 import { partnersReducer } from '../features/partners/partnersSlice'
 import { promotionsReducer } from '../features/promotions/promotionsSlice'
 import { favoritesReducer } from '../features/favorites/favoritesSlice'
+import {
+  persistStore,
+  persistCombineReducers,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const config = {
+  key: 'root',
+  storage: AsyncStorage,
+  debug: true //console logs messages to help with debugging
+}
+
 
 // keys in the store can be accessed by using useSelector and calling state.<key> eg: state.campsite
-
 export const store = configureStore({
-  reducer: {
+  reducer: persistCombineReducers(config, {
     campsites: campsitesReducer,
-    comments: commentsReducer, 
+    comments: commentsReducer,
     partners: partnersReducer,
     promotions: promotionsReducer,
     favorites: favoritesReducer
-  }
+  }),
+  middleware: (getDefaultMiddleware) => //this function ignores several action types redux persist will dispatch to the store
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER
+        ]
+      }
+    })
 })
+
+export const persistor = persistStore(store)
